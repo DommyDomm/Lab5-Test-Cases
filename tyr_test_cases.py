a902c76c40da13a4587cf9f96c083e5e907d4459
DommyDomm <130618174+DommyDomm@users.noreply.github.com>
2025-09-17 17:00:39 -0500

    Update tyr_test_cases.py

--- tyr_test_cases.py
+++ tyr_test_cases.py
@@ -10,8 +10,6 @@ sex:F age:99 cho:300 smo:N hdl:30 sbp:180 med:Y out:>30
 sex:M age:12 cho:100 smo:N hdl:100 sbp:90 med:Y out:<1
 sex:F age:16 cho:110 smo:Y hdl:99 sbp:100 med:N out:<1
 
-# Edge Case for invalid inputs
-sex:Female age:99 cho:300 smo:N hdl:30 sbp:180 med:Y out:>30
 # F, N, N
 # F, N, Y
 # F, Y, N
