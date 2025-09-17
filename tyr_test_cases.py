ef1bd747596ddec6c5f17bdbe1a4b12d72b20703
leahg25 <leahgraham566@gmail.com>
2025-09-17 11:53:00 -0500

    Update tyr_test_cases.py
    male female

--- tyr_test_cases.py
+++ tyr_test_cases.py
@@ -12,3 +12,43 @@ sex:F age:16 cho:110 smo:Y hdl:99 sbp:100 med:N out:<1
 
 # Edge Case for invalid inputs
 sex:Female age:99 cho:300 smo:N hdl:30 sbp:180 med:Y out:>30
+# F, N, N
+# F, N, Y
+# F, Y, N
+# F, Y, Y
+# M, N, N
+# M, N, Y
+# M, Y, N
+# M, Y, Y
+#different variations of yes or no / t/f
+sex:F age:40 cho:105 smo:N hdl:60 sbp:100 med:N out:<1
+sex:F age:40 cho:105 smo:N hdl:60 sbp:100 med:Y out:<1
+sex:F age:40 cho:105 smo:Y hdl:60 sbp:100 med:Y out:<1
+sex:F age:40 cho:105 smo:Y hdl:60 sbp:100 med:N out:<1
+
+sex:M age:40 cho:105 smo:N hdl:60 sbp:100 med:N out:<1
+sex:M age:40 cho:105 smo:N hdl:60 sbp:100 med:Y out:<1
+sex:M age:40 cho:105 smo:Y hdl:60 sbp:100 med:Y out:<1
+sex:M age:40 cho:105 smo:Y hdl:60 sbp:100 med:N out:<1
+# different age ranges
+# 20-34
+# 35-39
+# 40-44
+# 45-49
+# 50-54
+# 55-59
+# 60-64
+# 65-69 
+# 70-74 
+# 75-79
+sex:F age:40 cho:105 smo:N hdl:60 sbp:100 med:N out:<1
+sex:F age:40 cho:105 smo:N hdl:60 sbp:100 med:Y out:<1
+sex:F age:40 cho:105 smo:Y hdl:60 sbp:100 med:Y out:<1
+sex:F age:40 cho:105 smo:Y hdl:60 sbp:100 med:N out:<1
+
+sex:M age:40 cho:105 smo:N hdl:60 sbp:100 med:N out:<1
+sex:M age:40 cho:105 smo:N hdl:60 sbp:100 med:Y out:<1
+sex:M age:40 cho:105 smo:Y hdl:60 sbp:100 med:Y out:<1
+sex:M age:40 cho:105 smo:Y hdl:60 sbp:100 med:N out:<1
+
+
